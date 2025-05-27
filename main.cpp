4d9dd0f263c1a7dc978db5d3812d57999989798e
lucasvitor08 <lucasvitordev@gmail.com>
2025-05-26 22:16:49 -0300

    Função de cadastrar e listar vendas.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <locale.h>
 
-#define MAX_VENDAS 5
+#define MAX_VENDAS 50
 
 struct Usuarios {
-	char nome[50]; 
+	char usuario[50]; 
 	char genero[50]; 
 	char email[50]; 
 	char senha[50]; 
@@ -15,7 +15,8 @@ struct Usuarios {
 
 struct Empresa {
 	char nomeEmpresa[50]; 
-	int porteEmpresa; //poupa o cliente de ter que digitar muito!
+	char responsavel[50]; 
+	int porteEmpresa;
 	int tempoAtuacao;
 	float rendaMensal;
 	float metaLucro;
@@ -25,42 +26,14 @@ struct Venda {
 	char categoriaProduto[30];
 	int quantidade;
 	float precoUnidade;
+	char formaPagamento[20];
+	char tipoOperacao[15];
 };
 
 struct Venda venda[MAX_VENDAS];
 int qtdVendas = 0;
 
-void menuInicial() {
-	int op;
-	
-	while(1) {
-		printf("1- Login\n");
-		printf("2- Cadastrar novo usuário\n");
-		printf("3- Sair do sistema\n\n");
-		printf("Escolha uma opção: ");
-		scanf("%d", &op);
-		
-		system("cls");
-		
-		switch(op) {
-			case 1:
-				//login();
-				break;
-			case 2:
-				//cadastrar();
-				break;
-			case 3:
-				printf("Encerrando sistema...\n\n");
-				abort();				
-			default:
-				printf("Opção inválida!!\n\n");
-		}
-		
-		system("pause");
-		system("cls");
-	}
-}
-void tipoOperacao(int op) {
+void tipoOperacao(int op, int vendaIndex) {
 	system("cls");
 	
 	printf("1. Crédito.\n");
@@ -68,10 +41,21 @@ void tipoOperacao(int op) {
 	printf("Informe o tipo de operação desejado: ");
 	scanf("%d", &op);
 	
-	printf("\nVenda cadastrada com sucesso!!!\n\n");
+	switch(op) {
+		case 1:
+			printf("\nVenda cadastrada com sucesso!!!\n\n");
+			strcpy(venda[vendaIndex].tipoOperacao, "Crédito");
+			break;
+		case 2:
+			printf("\nVenda cadastrada com sucesso!!!\n\n");
+			strcpy(venda[vendaIndex].tipoOperacao, "Débito");
+			break;
+		default:
+			printf("Erro! Escolha uma opção válida.\n\n");
+	}
 }
 
-void formaPagamento(int op) {
+void formaPagamento(int op, int vendaIndex) {
 	int op2;
 	
 	system("cls");
@@ -85,19 +69,27 @@ void formaPagamento(int op) {
 	
 	switch(op) {
 		case 1:
-			tipoOperacao(op2);
+			strcpy(venda[vendaIndex].formaPagamento, "Cartão");
+			strcpy(venda[vendaIndex].tipoOperacao, "Cartão");
+			tipoOperacao(op, vendaIndex);
 			break;
 		case 2:
 			printf("\nVenda cadastrada com sucesso!!!\n\n");
+			strcpy(venda[vendaIndex].formaPagamento, "Boleto");
+			strcpy(venda[vendaIndex].tipoOperacao, "N/A");
 			break;
 		case 3:
 			printf("\nVenda cadastrada com sucesso!!!\n");
+			strcpy(venda[vendaIndex].formaPagamento, "Pix");
+			strcpy(venda[vendaIndex].tipoOperacao, "N/A");
 			break;
 		case 4:
 			printf("\nVenda cadastrada com sucesso!!!\n");
+			strcpy(venda[vendaIndex].formaPagamento, "Dinheiro");
+			strcpy(venda[vendaIndex].tipoOperacao, "N/A");
 			break;
 		default:
-			printf("\nEscolha uma opção válida!\n\n");
+			printf("\nErro! Escolha uma opção válida!\n\n");
 	}
 }
 
@@ -113,27 +105,10 @@ void cadastrarVenda() {
 		return;
 	}
 	
-	while(1) {
-		fflush(stdin);
-		printf("Informe a categoria do produto: ");
-		fgets(novaCategoria, 30, stdin);
-		novaCategoria[strcspn(novaCategoria, "\n")] = '\0';
-	
-		int categoriaExistente = 0;
-		for (int i = 0 ; i < qtdVendas ; i++) {
-			if (strcmp(venda[i].categoriaProduto, novaCategoria) == 0) {
-				categoriaExistente = 1;
-				break;
-			}
-		}
-	
-		if(categoriaExistente) {
-			printf("Esta categoria já existe! Tente novamente.\n\n");
-		} else {
-			strcpy(venda[qtdVendas].categoriaProduto, novaCategoria);
-			break;
-		}
-	}
+	fflush(stdin);
+	printf("Informe a categoria do produto: ");
+	fgets(venda[qtdVendas].categoriaProduto, 30, stdin);
+	venda[qtdVendas].categoriaProduto[strcspn(venda[qtdVendas].categoriaProduto, "\n")] = '\0';
 	
 	printf("Informe a quantidade do produto: ");
 	scanf("%d", &venda[qtdVendas].quantidade);
@@ -143,9 +118,8 @@ void cadastrarVenda() {
 	
 	system("cls");
 	
+	formaPagamento(0, qtdVendas);
 	qtdVendas++;
-	
-	formaPagamento(op);
 	
 	fflush(stdin);
 	printf("Deseja cadastrar mais alguma venda? (S/N)\n");
@@ -155,22 +129,21 @@ void cadastrarVenda() {
 	} while (op2 == 'S' || op2 == 's');
 }
 
-void menuPainel() {
+void menuCadastrar() {
 	int op;
 	
 	while(1) {
-		printf("1- Cadastrar nova venda.\n");
-		printf("2- Cadastrar categoria de produtos.\n");
-		printf("3- Cadastrar sub-categoria de produtos.\n");
-		printf("4- Cadastrar funcionário.\n");
-		printf("5- Listar funcionários.\n");
-		printf("6- Listar vendas.\n");
-		printf("7- Listar categorias e sub-categorias.\n");
-		printf("8- Editar venda.\n");
-		printf("9- Remover venda.\n");
-		printf("10- Acessar métricas.\n");		
-		printf("11- Voltar ao menu.\n\n");		
-		printf("Escolha uma opção: ");
+		printf("| Menu - Cadastrar informações\n");
+		printf("---------------------------\n\n");
+		
+		printf("1- Cadastrar nova venda\n");
+		printf("2- Cadastrar cliente\n");
+		printf("3- Cadastrar produto\n");
+		printf("4- Cadastrar funcionário\n");
+		printf("5- Cadastrar categoria\n");
+		printf("6- Cadastrar subcategoria\n");
+		printf("7- Voltar ao menu\n\n");	
+		printf("-> Escolha uma opção: ");
 		scanf("%d", &op);
 		
 		system("cls");
@@ -180,35 +153,23 @@ void menuPainel() {
 				cadastrarVenda();
 				break;
 			case 2:
-				//cadastrarCategoria();
-				break;				
+				//cadastrarCliente();
+				break;
 			case 3:
-				//cadastrarSubCategoria();
-				break;				
+				//cadastrarProduto();
+				break;
 			case 4:
 				//cadastrarFuncionario();
-				break;				
+				break;
 			case 5:
-				//listarFuncionarios();
-				break;				
+				//cadastrarCategoria();
+				break;
 			case 6:
-				//listarVendas();
-				break;				
+				//cadastrarSubcategoria();
+				break;
 			case 7:
-				//listarCategorias();
-				break;								
-			case 8:
-				//editarVenda();
-				break;				
-			case 9:
-				//removerVenda();
-				break;				
-			case 10:
-				//acessarMetricas();
-				break;				
-			case 11:
-				//voltarMenu();
-				break;				
+				printf("Voltando para o menu painel!\n\n");
+				return;
 			default:
 				printf("Escolha uma opção válida!\n\n");
 		}
@@ -218,10 +179,197 @@ void menuPainel() {
 	}
 }
 
+void listarVendas() {
+	if(qtdVendas == 0) {
+		printf("Não há nenhuma venda cadastrada!\n\n");
+		return;
+	}
+	
+	printf("LISTAGEM DE VENDAS\n\n");
+	for(int i = 0 ; i < qtdVendas ; i++) {
+		printf("Venda #%d: \n", i + 1);
+		printf("Categoria: %s.\n", venda[i].categoriaProduto);
+		printf("Quantidade: %d.\n", venda[i].quantidade);
+		printf("Preço p/unidade: %.2f.\n\n", venda[i].precoUnidade);
+		printf("Forma de pagamento: %s.\n", venda[i].formaPagamento);
+    printf("Tipo de operação: %s.\n\n", venda[i].tipoOperacao);
+		printf("---------------------------------\n\n");
+	}
+}
+
+void menuListar() {
+	int op;
+	
+	while(1) {
+		printf("| Menu - Listar informações\n");
+		printf("---------------------------\n\n");
+		
+		printf("1- Listar produtos\n");
+		printf("2- Listar clientes\n");
+		printf("3- Listar funcionários\n");
+		printf("4- Listar vendas\n");
+		printf("5- Listar categorias\n");
+		printf("6- Voltar ao menu\n\n");		
+		printf("-> Escolha uma opção: ");
+		scanf("%d", &op);
+		
+		system("cls");
+		
+		switch(op) {
+			case 1:
+				//listarProdutos();
+				break;
+			case 2:
+				//listarClientes();
+				break;
+			case 3:
+				//listarFuncionario();
+				break;
+			case 4:
+				listarVendas();
+				break;
+			case 5:
+				//listarCategorias();
+				break;
+			case 6:
+				printf("Voltando para o menu painel!\n\n");
+				return;
+			default:
+				printf("Escolha uma opção válida!\n\n");
+		}
+		
+		system("pause");
+		system("cls");
+	}
+}
+
+void menuEditar() {
+	int op;
+	
+	while(1) {
+		printf("| Menu - Editar informações\n");
+		printf("---------------------------\n\n");
+		
+		printf("1- Editar venda\n");
+		printf("2- Remover venda\n");
+		printf("3- Voltar ao menu\n\n");
+		printf("-> Escolha uma opção: ");
+		scanf("%d", &op);
+		
+		system("cls");
+		
+		switch(op) {
+			case 1:
+				//editarVenda();
+				break;
+			case 2:
+				//removerVenda();
+				break;
+			case 3:
+				printf("Voltando para o menu painel!\n\n");
+				return;
+			default:
+				printf("Escolha uma opção válida!\n\n");
+		}
+		
+		system("pause");
+		system("cls");
+	}
+}
+
+void exibirRelatorio() {
+	//para desenvolver
+}
+
+int menuPainel() {
+	int op;
+	
+	while(1) {
+		printf("| Menu Painel\n");
+		printf("---------------------------\n\n");				
+		
+		printf("1- Cadastrar informações\n");				
+		printf("2- Listar informações\n");			
+		printf("3- Editar\n");		
+		printf("4- Acessar relatório\n");		
+		printf("5- Voltar ao menu\n\n");		
+		printf("-> Escolha uma opção: ");
+		scanf("%d", &op);
+		
+		system("cls");
+		
+		switch(op) {
+			case 1:
+				menuCadastrar();
+				break;
+			case 2:
+				menuListar();
+				break;				
+			case 3:
+				menuEditar();
+				break;				
+			case 4:
+				printf("Relatório acessado!\n\n");
+				//exibirRelatorio();
+				break;				
+			case 5:
+				printf("Voltando para o menu!\n\n");
+				return 2;
+				break;					
+			default:
+				printf("Escolha uma opção válida!\n\n");
+		}
+		
+		system("pause");
+		system("cls");
+	}
+}
+
+void login() {
+	printf("Login acessado! Redirecionando para o Menu Painel.\n\n");
+	
+	system("pause");	
+	system("cls");	
+	
+	menuPainel();
+}
+
+void menuInicial() {
+	int op;	
+	
+	while(1) {
+		printf("------ Menu login/cadastro ------\n\n");
+		printf("1- Login\n");
+		printf("2- Cadastrar novo usuário\n");
+		printf("3- Sair do sistema\n\n");
+		printf("-> Escolha uma opção: ");
+		scanf("%d", &op);
+		
+		system("cls");
+		
+		switch(op) {
+			case 1:
+				login();
+				break;
+			case 2:
+				//cadastrar();
+				break;
+			case 3:
+				printf("Encerrando sistema...\n\n");
+				abort();				
+			default:
+				printf("Opção inválida!!\n\n");
+		}
+		
+		system("pause");
+		system("cls");
+	}
+	
+	if(menuPainel() == 2) menuInicial(); 
+}
 
 int main() {
 	setlocale(LC_ALL, "portuguese");
 	
-	//menuInicial();
-	menuPainel();
+	menuInicial();
 }
